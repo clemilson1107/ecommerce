ad74b8a44a187bb5d6a0a24572adc9d569e350bb
Clemilson <clemilson.ferreira_@hotmail.com>
2018-07-08 02:09:53 -0300

    tpladmin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,12 +10,12 @@ class Page{
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tplDir = "/views/"){
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"	=> $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"tpl_dir"	=> $_SERVER["DOCUMENT_ROOT"].$tplDir,
 			"cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"		=> false
 		);
